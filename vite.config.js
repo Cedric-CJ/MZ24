9982d7d7837ef10e99e1f0a39c7aed10a55f518a
Cedric Arnhold <unicedric@gmail.com>
2024-07-29 20:24:25 +0200

    test for deploy on Github

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://cedric-cj.github.io/MZ24/',
+  base: '/',
   plugins: [
     vue(),
   ],
